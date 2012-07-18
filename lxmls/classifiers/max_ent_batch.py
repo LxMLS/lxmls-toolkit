e5afbc47ac199db558d54bd5d528d6eb840e4879
Joao Graca <gracaninja@gmail.com>
2012-07-18 14:17:56 +0100

    corrected some errors on day 1 wth imports

--- lxmls/classifiers/max_ent_batch.py
+++ lxmls/classifiers/max_ent_batch.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 import scipy.optimize.lbfgsb as opt2
-from my_math_utils import *
+from util.my_math_utils import *
 import linear_classifier as lc
 
 ################
