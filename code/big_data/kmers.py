4761c6c97c2ca597d85df84abc606247d8112b34
Luis Pedro Coelho <luis@luispedro.org>
2013-06-28 15:20:08 +0200

    BUG Remove extra check
    The data has already been cleaned of non-words

--- code/big_data/kmers.py
+++ code/big_data/kmers.py
@@ -2,21 +2,16 @@
 from mrjob.job import MRJob
 import re
 
-# Check whether the word is a real word
-def _is_valid_word(w):
-    return re.search(r'\W', w) is None
-
 class TrimerCount(MRJob):
     def mapper(self, _, doc):
         c = {}
         # Process the document
         for i in xrange(len(doc)-3):
             w = doc[i:i+3]
-            if _is_valid_word(w):
-                if w in c:
-                    c[w] += 1
-                else:
-                    c[w] = 1
+            if w in c:
+                c[w] += 1
+            else:
+                c[w] = 1
 
         # Now, output the results
         for w,c in c.items():
