26d65e43d5411a968d534abb7749e36591c695dd
zita <zita.marinho@ist.utl.pt>
2013-07-13 23:42:31 +0100

    day 4 code revised. prints changed:

--- code/labs/day4.py
+++ code/labs/day4.py
@@ -1,4 +1,4 @@
-###### Exercises for pratica class 1
+###### Exercises for lab day 4 Parsing
 import sys
 sys.path.append("parsing/" )
 
@@ -8,17 +8,22 @@ import pdb
 
 
 
-print "Exercise 6.1"
+print "Exercise 4.4.1"
 
 dp = depp.DependencyParser()
 
 dp.read_data("portuguese")
+pdb.set.trace()
+
+goon = raw_input("Enter to go on to next exercise:")
+
+print "Exercise 4.4.2"
 dp.train_perceptron(10)
 dp.test()
 
 goon = raw_input("Enter to go on to next exercise:")
 
-print "Exercise 6.2"
+print "Exercise 4.4.3"
 
 dp.features.use_lexical = True
 dp.read_data("portuguese")
@@ -37,14 +42,14 @@ dp.test()
 
 goon = raw_input("Enter to go on to next exercise:")
 
-print "Exercise 6.3"
+print "Exercise 4.4.4"
 
 dp.train_crf_sgd(10, 0.01, 0.1)
 dp.test()
 
 goon = raw_input("Enter to go on to next exercise:")
 
-print "Exercise 6.4"
+print "Exercise 4.4.5"
 
 dp.read_data("english")
 dp.train_perceptron(10)
@@ -52,7 +57,7 @@ dp.test()
 
 goon = raw_input("Enter to go on to next exercise:")
 
-print "Exercise 6.5"
+print "Exercise 4.4.6"
 
 dp = depp.DependencyParser()
 dp.features.use_lexical = True
