e4c2754e157d89b341f45ac19df751746609762b
Luis Pedro Coelho <luis@luispedro.org>
2013-07-17 10:12:13 +0200

    MIN Add Lab 6 script

--- lxmls/labs/day6.sh
+++ lxmls/labs/day6.sh
@@ -9,12 +9,8 @@ rm -f parameters.txt
 for ((i=0; i<${num_iterations}; i++ ))
 do
     # Note: add "-r local" when the lxmls module is installed.
-    python big_data_em/parallel_em.py < encoded.txt > parameters_new.txt
-    mv parameters_new.txt parameters.txt
+    python big_data_em/emstep.py -r local --file=word_tag_dict.pkl < encoded.txt > parameters_new.txt
+    mv parameters_new.txt hmm.txt
     echo "Iteration $i"
-    grep "Log-likelihood" parameters.txt
 done
 
-# Run distributed EM on Amazon
-# IMPORTANT: you must export your AWS_ACCESS_KEY and AWS_SECRET_KEY variables before running this!
-# python big_data_em/parallel_em.py -r emr --num-ec2-instances 2 --aws-region eu-west-1
