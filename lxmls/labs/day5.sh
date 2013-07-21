d55d036ff383004e0903bab7ff30d8ea0e848e9b
Andre Martins <andre.t.martins@gmail.com>
2013-07-21 19:24:51 +0100

    Renamed kmers.py to trimercount.py para ser consistente com o guia.

--- lxmls/labs/day5.sh
+++ lxmls/labs/day5.sh
@@ -8,8 +8,8 @@ python big_data/wordcount.py ../data/wikipedia/en_perline001.txt > results_local
 # python big_data/wordcount.py -r emr ../data/wikipedia/en_perline001.txt --num-ec2-instances 2 --aws-region eu-west-1 > results_AWS_wordcount.txt
 
 # Run language detection locally
-python big_data/kmers.py ../data/wikipedia/en_perline001.txt > output.en.txt
-python big_data/kmers.py ../data/wikipedia/pt_perline01.txt > output.pt.txt
+python big_data/trimercount.py ../data/wikipedia/en_perline001.txt > output.en.txt
+python big_data/trimercount.py ../data/wikipedia/pt_perline01.txt > output.pt.txt
 python big_data/postprocess.py
 
 # Run language detection on Amazon
