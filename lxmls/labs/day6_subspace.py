12cb085646f71fdfe99e12dc86dae9cdc529866c
samir <silvio.aam@gmail.com>
2015-07-14 23:36:16 +0100

    Deep Learning day: added pre-trained word embeddings;  added exercise with maxent model on semeval dataset; other small revisions

--- lxmls/labs/day6_subspace.py
+++ lxmls/labs/day6_subspace.py
@@ -12,7 +12,6 @@ from ipdb import set_trace
 #           CONFIG 
 ####################################
 
-
 DO_TRAIN = 1
 # TRAIN CONFIGURATION
 n_iter  = 6
@@ -26,12 +25,12 @@ t2015_x, t2015_y = sr.tweets2015
 t2014_x, t2014_y = sr.tweets2014
 t2013_x, t2013_y = sr.tweets2013
 
+#reformat the labels
 dev_y   = [np.array(dy).astype('int32')[None] for dy in dev_y]
 train_y = [np.array(ty).astype('int32')[None] for ty in train_y]
 t2015_y = [np.array(ty).astype('int32')[None] for ty in t2015_y]
 t2014_y = [np.array(ty).astype('int32')[None] for ty in t2014_y]
 t2013_y = [np.array(ty).astype('int32')[None] for ty in t2013_y]
-emb_path = 'data/twitter/features/E.pkl'
 
 # SUBSPACE EMBEDDING
 subsize    = 10
@@ -39,7 +38,10 @@ model_path = 'data/twitter/features/semeval.pkl'
 
 # Create or load model
 if DO_TRAIN:
-    nn  = embSubMLP(emb_path, n_h1=subsize)
+    #get the pretrained embeddings
+    E = sr.get_embedding()
+    E = E.astype(theano.config.floatX)
+    nn  = embSubMLP(E, n_h1=subsize)
     print "Training %s" % model_path    
     nn.train((train_x,train_y), (dev_x,dev_y), lrate, n_iter)
 else:
@@ -49,13 +51,17 @@ else:
 #              TEST
 ####################################
 
-Fmes_train, acc_train = nn.eval(train_x, train_y)
-Fmes_t2013, acc_t2013 = nn.eval(t2013_x, t2013_y)
-Fmes_t2014, acc_t2014 = nn.eval(t2014_x, t2014_y)
-Fmes_t2015, acc_t2015 = nn.eval(t2015_x, t2015_y)
+print "---TESTING---"
+print "\nTrain"
+Fmes_train, acc_train = nn.evaluate(train_x, train_y)
+print "\nTweets 2013"
+Fmes_t2013, acc_t2013 = nn.evaluate(t2013_x, t2013_y)
+print "\nTweets 2014"
+Fmes_t2014, acc_t2014 = nn.evaluate(t2014_x, t2014_y)
+print "\nTweets 2015"
+Fmes_t2015, acc_t2015 = nn.evaluate(t2015_x, t2015_y)
 
-
-print "\nAvg. F-measure (POS/NEG)"
+print "\n\nAvg. F-measure (POS/NEG)"
 print "train: %.3f\ntweets 2015: %.3f\ntweets 2014: %.3f\ntweets 2013: %.3f\n"%(Fmes_train, Fmes_t2015, Fmes_t2014, Fmes_t2013)
 
 print "\nAccuracy"
