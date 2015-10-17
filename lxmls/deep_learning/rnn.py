9acd7ba16c96bb32ec3b740c3699b3548fb6a9bf
Ramón Astudillo <ramon@astudillo.com>
2015-10-17 12:22:22 +0100

    Version working with senna_50 embeddings

--- lxmls/deep_learning/rnn.py
+++ lxmls/deep_learning/rnn.py
@@ -8,24 +8,27 @@ from ipdb import set_trace
 
 class RNN():
 
-    def __init__(self, n_words, n_hidd, n_tags):
+    def __init__(self, n_words, n_emb, n_hidd, n_tags):
 
         # MODEL PARAMETERS
-        W_x = np.random.uniform(size=(n_hidd, n_words)) # Input layer 
+        W_e = np.random.uniform(size=(n_emb, n_words))  # Input layer 
+        W_x = np.random.uniform(size=(n_hidd, n_emb))   # Input layer 
         W_h = np.random.uniform(size=(n_hidd, n_hidd))  # Recurrent layer
         W_y = np.random.uniform(size=(n_tags, n_hidd))  # Output layer
         # Cast to theano GPU-compatible type
+        W_e = W_e.astype(theano.config.floatX)
         W_x = W_x.astype(theano.config.floatX)
         W_h = W_h.astype(theano.config.floatX)
         W_y = W_y.astype(theano.config.floatX)
         # Store as shared parameters
+        _W_e = theano.shared(W_e, borrow=True)
         _W_x = theano.shared(W_x, borrow=True)
         _W_h = theano.shared(W_h, borrow=True)
         _W_y = theano.shared(W_y, borrow=True)
 
         # Class variables
         self.n_hidd = n_hidd
-        self.param  = [_W_x, _W_h, _W_y]
+        self.param  = [_W_e, _W_x, _W_h, _W_y]
 
     def _forward(self, _x, _h0=None):
 
@@ -35,21 +38,24 @@ class RNN():
             _h0 = theano.shared(h0, borrow=True)
 
         # COMPUTATION GRAPH
-        _W_x, _W_h, _W_y = self.param
+        _W_e, _W_x, _W_h, _W_y = self.param
 
         # NOTE: Since _x contains the indices rather than full one-hot vectors,
-        # we use  _W_x[:, x_tm1].T instead of T.dot(x_tm1, _W_x.T)
+        # use _W_e[:, _x].T instead of T.dot(_x, _W_e.T)
 
         ###########################
         # Solution to Exercise 6.3 
+
+        # Embedding layer 
+        _z1 = _W_e[:, _x].T
     
         # This defines what to do at each step
-        def rnn_step(x_tm1, h_tm1, _W_x, W_h):
-            return T.nnet.sigmoid(_W_x[:, x_tm1].T + T.dot(h_tm1, W_h.T))
+        def rnn_step(_x_tm1, _h_tm1, _W_x, W_h):
+            return T.nnet.sigmoid(T.dot(_x_tm1, _W_x.T) + T.dot(_h_tm1, W_h.T))
     
         # This creates the variable length computation graph (unrols the rnn)
         _h, updates = theano.scan(fn=rnn_step, 
-                                  sequences=_x, 
+                                  sequences=_z1, 
                                   outputs_info=dict(initial=_h0),
                                   non_sequences=[_W_x ,_W_h])
     
@@ -65,52 +71,82 @@ class RNN():
         return _p_y
 
 
-def lstm(_x, _h0, _c0, _W_x, _W_h, _W_c, H):
+class LSTM():
 
-    '''
-    LSTM Recurrent Neural Network. 
+    def __init__(self, n_words, n_emb, n_hidd, n_tags):
 
-    x, h0, c0      theano.tensor.matrix of shapes [L, I], [1, H], [1, H]
-                   x is the sequence input to the network, h0 and c0 initial
-                   values for the hidden variables.
-                   L = number of samples, I = number of features, H = size of
-                   hidden 
-    W_x, W_h, W_c  theano.shared variables of shapes [4*H, I], [4*H, H], [4*H, H]
-                   These are the weights of the LSTM. Note that all
-                   transformations per variable are stacked for efficiency
+        # MODEL PARAMETERS
+        W_e = np.random.uniform(size=(n_emb, n_words))  # Word embeeding layer 
+        W_x = np.random.uniform(size=(n_hidd, n_emb))   # RNN Input layer
+        W_h = np.random.uniform(size=(n_hidd, n_hidd))  # RNN Recurrent layer 
+        W_c = np.random.uniform(size=(n_hidd, n_hidd))  # Second recurrent var 
+        W_y = np.random.uniform(size=(n_tags, n_hidd))  # Output layer
+        # Cast to theano GPU-compatible type
+        W_e = W_e.astype(theano.config.floatX)
+        W_x = W_x.astype(theano.config.floatX)
+        W_h = W_h.astype(theano.config.floatX)
+        W_c = W_c.astype(theano.config.floatX)
+        W_y = W_y.astype(theano.config.floatX)
+        # Store as shared parameters
+        _W_x = theano.shared(W_x, borrow=True)
+        _W_h = theano.shared(W_h, borrow=True)
+        _W_c = theano.shared(W_c, borrow=True)
+        _W_y = theano.shared(W_y, borrow=True)
 
-    '''
+        # Class variables
+        self.n_hidd = n_hidd
+        self.param  = [_W_x, _W_h, _W_c, _W_y]
 
-    # Per loop operation 
-    def _step(_x_tm1, _h_tm1, _c_tm1, _W_x, _W_h, _W_c):
+    def _forward(self, _x, _h0=None, _c0=None):
+
+        # Default initial hidden is allways set to zero
+        if _h0 is None:
+            h0  = np.zeros((1, self.n_hidd)).astype(theano.config.floatX)
+            _h0 = theano.shared(h0, borrow=True)
+        if _c0 is None:
+            c0  = np.zeros((1, self.n_hidd)).astype(theano.config.floatX)
+            _c0 = theano.shared(c0, borrow=True)
+
+        # COMPUTATION GRAPH
+        _W_x, _W_h, _W_c, _W_y = self.param
+        H                      = self.n_hidd
+
+        # Per loop operation 
+        def _step(_x_tm1, _h_tm1, _c_tm1, _W_x, _W_h, _W_c):
+            
+            # LINEAR TRANSFORMS
+            # Note that all transformations per variable are stacked for efficiency
+            # each individual variable is then selected using slices of H size
+            # (see below)
+
+            set_trace()
+
+            _z_x = _W_x[:, _x_tm1].T
+            _z_h = T.dot(_h_tm1, _W_h.T)
+            _z_c = T.dot(_c_tm1, _W_c.T)
         
-        # LINEAR TRANSFORMS
-        # Note that all transformations per variable are stacked for efficiency
-        # each individual variable is then selected using slices of H size
-        # (see below)
-        _z_x = T.dot(_x_tm1, _W_x.T)
-        _z_h = T.dot(_h_tm1, _W_h.T)
-        _z_c = T.dot(_c_tm1, _W_c.T)
+            # GATES
+            # Note the subtlety: _x_tm1 and hence _z_x are flat and have size (I,),
+            # _h_tm1 and _c_tm1 are not and thus have size (1, H)
+            _i_t = T.nnet.sigmoid(_z_x[:H] +_z_h[:, :H] +_z_c[:, :H])
+            _f_t = T.nnet.sigmoid(_z_x[H:2*H] +_z_h[:, H:2*H] +_z_c[:, H:2*H])
+            _o_t = T.nnet.sigmoid(_z_x[3*H:4*H] +_z_h[:, 3*H:4*H] +_z_c[:, 2*H:3*H])
+        
+            # HIDDENS
+            _c_t = _f_t*_c_tm1 + _i_t*T.tanh(_z_x[2*H:3*H] +_z_h[:, 2*H:3*H])    
+            _h_t = _o_t*T.tanh(_c_t)
+        
+            return _h_t, _c_t
     
-        # GATES
-        # Note the subtlety: _x_tm1 and hence _z_x are flat and have size (H,),
-        # _h_tm1 and _c_tm1 are not and thus have size (1, H)
-        _i_t = T.nnet.sigmoid(_z_x[:H] +_z_h[:, :H] +_z_c[:, :H])
-        _f_t = T.nnet.sigmoid(_z_x[H:2*H] +_z_h[:, H:2*H] +_z_c[:, H:2*H])
-        _o_t = T.nnet.sigmoid(_z_x[3*H:4*H] +_z_h[:, 3*H:4*H] +_z_c[:, 2*H:3*H])
-    
-        # HIDDENS
-        _c_t = _f_t*_c_tm1 + _i_t*T.tanh(_z_x[2*H:3*H] +_z_h[:, 2*H:3*H])    
-        _h_t = _o_t*T.tanh(_c_t)
-    
-        return _h_t, _c_t
+        # Unrol the loop
+        _h, updates = theano.scan(_step,
+                                  sequences=_x,
+                                  outputs_info=[_h0, _c0],
+                                  non_sequences=[_W_x, _W_h, _W_c])
+        # Just keep the first hidden, remove intermediate empty dimension
+        _z2 = _h[0][:, 0, :]
 
-    # Unrol the loop
-    _h, updates = theano.scan(_step,
-                              sequences=_x,
-                              outputs_info=[_h0, _c0],
-                              non_sequences=[_W_x, _W_h, _W_c])
-    # Just keep the first hidden, remove intermediate empty dimension
-    _h = _h[0][:, 0, :]
+        # Output layer
+        _p_y = T.nnet.softmax(T.dot(_z2, _W_y.T))
 
-    return _h, updates
+        return _p_y
--- lxmls/labs/day6.py
+++ lxmls/labs/day6.py
@@ -50,6 +50,33 @@ dev_seq   = corpus.read_sequence_list_conll("data/dev-22.conll",
 train_seq, test_seq, dev_seq = pcc.compacify(train_seq, test_seq, dev_seq,
                                              theano=True)
 
+def extract_embeddings(embedding_path):
+    with open(embedding_path) as fid:
+        for i, line in enumerate(fid.readlines()):
+            # Initialize
+            if i == 0:
+                 N    = len(line.split()[1:])     
+                 E    = np.random.uniform(size=(N, len(train_seq.x_dict)))
+                 n    = 0
+            word = line.split()[0].lower() 
+            if word[0].upper() + word[1:] in train_seq.x_dict:
+                idx        = train_seq.x_dict[word[0].upper() + word[1:]]
+                E[:, idx]  = np.array(line.strip().split()[1:]).astype(float)
+                n         += 1
+            elif word in train_seq.x_dict:
+                idx        = train_seq.x_dict[word]
+                E[:, idx]  = np.array(line.strip().split()[1:]).astype(float)
+                n         += 1
+            print "\r%d/%d" % (n, len(train_seq.x_dict)),    
+    print "Embeddings have %2f%% OOV" % ((1-n*1./len(train_seq.x_dict))*100)
+    return E
+
+# Extract embeddings for the sentences
+import numpy as np
+embedding_path = 'senna_50'
+#embedding_path = '/ffs/tmp/ramon/cajon/NLSE.DATA/txt/wiki.sskipngram.600'
+E              = extract_embeddings(embedding_path)
+
 #
 # CREATE RNN TO PREDICT POS TAGS 
 #
@@ -57,91 +84,124 @@ train_seq, test_seq, dev_seq = pcc.compacify(train_seq, test_seq, dev_seq,
 import numpy as np
 import theano
 import theano.tensor as T
-import lxmls.deep_learning.rnn as rnn
+import lxmls.deep_learning.rnn as rnns
 
-# FORWARD
+#
+# DEFINE MODEL
+#
+
 # CONFIG 
-n_words = len(train_seq.x_dict.keys())     # Number of words
-n_hidd  = 10                               # Size of the recurrent layer
-n_tags  = len(train_seq.y_dict.keys())     # Number of POS tags
+#n_words = E.shape[0]                       # Number of words
+#n_emb   = E.shape[1]                       # Number of words
+n_words = len(train_seq.x_dict)             # Number of words in vocabulary
+n_emb   = 50                                # Size of the embedding layer
+n_hidd  = 10                                # Size of the recurrent layer
+n_tags  = len(train_seq.y_dict.keys())      # Number of POS tags
 # SYMBOLIC VARIABLES
-_x      = T.ivector('x')                   # Input words indices
-_y      = T.ivector('y')                   # True output tags indices
+_x      = T.ivector('x')                    # Input words indices
 # Define the RNN
-rnn     = rnn.RNN(n_words, n_hidd, n_tags)
+rnn     = rnns.RNN(n_words, n_emb, n_hidd, n_tags)
 # Forward
 _p_y    = rnn._forward(_x)
+
+# Set embeddings
+rnn.param[0].set_value(E)
+
+#
+# DEFINE TRAINING 
+#
+
+# CONFIG
+lrate   = 0.5  # Learning rate          
+n_iter  = 30   # Number of iterations
+# SYMBOLIC VARIABLES
+_y      = T.ivector('y')                   # True output tags indices
 # Train cost
 _F      = -T.mean(T.log(_p_y)[T.arange(_y.shape[0]), _y]) 
 # Total prediction error 
 _err    = T.sum(T.neq(T.argmax(_p_y,1), _y))
-err_sum = theano.function([_x, _y], _err)
 
 # SGD UPDATE RULE
-lrate   = 0.5
 updates = [(_par, _par - lrate*T.grad(_F, _par)) for _par in rnn.param] 
 
-# COMPILE FORWARD, BATCH UPDATE
-fwd          = theano.function([_x], _p_y)
+# COMPILE ERROR FUNCTION, BATCH UPDATE
+err_sum      = theano.function([_x, _y], _err)
 batch_update = theano.function([_x, _y], _F, updates=updates)
 
-# DEVEL
-err = 0
-N   = 0
-for n, seq in enumerate(dev_seq):
-#    x      = np.array(train_seq[0].x).astype('int32')
-#    y      = np.array(train_seq[0].y).astype('int32')
-    err  += err_sum(seq.x, seq.y)
-    N     += seq.y.shape[0]
-print "Acc %2.2f %%" % (100*(1 - err*1./N))
+# Function computing accuracy for a sequence of sentences
+def accuracy(seq):
+    err = 0
+    N   = 0
+    for n, seq in enumerate(seq):
+        err += err_sum(seq.x, seq.y)
+        N   += seq.y.shape[0]
+    return 100*(1 - err*1./N) 
 
-for i in range(20):
+#
+# TRAIN MODEL WITH SGD
+#
 
-    # TRAIN EPOCH
+#TODO: Merge with the other SGD
+
+# EPOCH LOOP
+for i in range(n_iter):
+
+    # SENTENCE LOOP
     cost = 0
-    err  = 0
-    N    = 0
     for n, seq in enumerate(train_seq):
-#        x     = np.array(seq.x).astype('int32')
-#        y     = np.array(seq.y).astype('int32')
-        err  += err_sum(seq.x, seq.y)
-        N    += seq.x.shape[0]
         cost += batch_update(seq.x, seq.y)
+        # INFO
         perc  = (n+1)*100./len(train_seq) 
         sys.stdout.write("\r%2.2f %%" % perc)
         sys.stdout.flush()
 
-    Acc = (1 - err*1./N)*100.
-    sys.stdout.write("\rTrain %d/%d cost %2.2f Acc %2.2f %%"  
-                     % (n+1, len(train_seq), cost, Acc))
-    sys.stdout.flush()
-    print ""
-    
-    # DEVEL
-    err = 0
-    N   = 0
-    for n, seq in enumerate(dev_seq):
-#        x    = np.array(seq.x).astype('int32')
-#        y    = np.array(seq.y).astype('int32')
-        err  += err_sum(seq.x, seq.y)
-        N   += seq.y.shape[0]
-    print "Devel Acc %2.2f %%" % (100*(1 - err*1./N))
+    # Accuracy on train and dev set
+    Acc = accuracy(train_seq)
+    print "\rEpoch %d: Train cost %2.2f Acc %2.2f %%" % (i+1, cost, Acc),
+    print " Devel Acc %2.2f %%" % accuracy(dev_seq)
 
-# TEST 
-err = 0
-N   = 0
-for n, seq in enumerate(test_seq):
-#    x      = np.array(seq.x).astype('int32')
-#    y      = np.array(seq.y).astype('int32')
-    err   += err_sum(seq.x, seq.y)
-    N     += seq.y.shape[0]
-print "Test Acc %2.2f %%" % (100*(1 - err*1./N))
+# Final accuracy on the dev set
+print "Test Acc %2.2f %%" % accuracy(test_seq)
 
-set_trace()
+exit()
 
 print "\n######################",
 print "\n   Exercise 6.4"
 print "######################"
 
 # Convince yourself that LSTMs and GRUs are just slightly more complex RNNs
-# Use here those nice pics from the blog-post
+# TODO: Use here those nice pics from the blog-post
+
+# Define the LSTM
+lstm = rnns.LSTM(n_words, n_hidd, n_tags)
+# Forward
+_p_y = lstm._forward(_x)
+# Train cost
+_F      = -T.mean(T.log(_p_y)[T.arange(_y.shape[0]), _y]) 
+# Total prediction error 
+_err    = T.sum(T.neq(T.argmax(_p_y,1), _y))
+
+# SGD UPDATE RULE
+updates = [(_par, _par - lrate*T.grad(_F, _par)) for _par in lstm.param] 
+
+# COMPILE ERROR FUNCTION, BATCH UPDATE
+err_sum      = theano.function([_x, _y], _err)
+batch_update = theano.function([_x, _y], _F, updates=updates)
+
+# EPOCH LOOP
+for i in range(n_iter):
+    cost = 0
+    for n, seq in enumerate(train_seq):
+        cost += batch_update(seq.x, seq.y)
+        # INFO
+        perc  = (n+1)*100./len(train_seq) 
+        sys.stdout.write("\r%2.2f %%" % perc)
+        sys.stdout.flush()
+
+    # Accuracy on train and dev set
+    Acc = accuracy(train_seq)
+    print "\rEpoch %d: Train cost %2.2f Acc %2.2f %%" % (i+1, cost, Acc),
+    print " Devel Acc %2.2f %%" % accuracy(dev_seq)
+
+# Final accuracy on the dev set
+print "Test Acc %2.2f %%" % accuracy(test_seq)
