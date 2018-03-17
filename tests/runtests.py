6451078309dbd0c0268e2eb2a9894224df88d6f0
Pedro Balage <pedrobalage@gmail.com>
2018-03-17 19:56:51 +0000

    organized unit tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -4,7 +4,7 @@ import sys
 import os
 import pytest
 
-LXMLS_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+LXMLS_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, LXMLS_ROOT)
 
 pytest.main(['test_day1.py', 'test_day2.py', 'test_day3.py', 'test_day4.py'])
