5f354b6ac49ba71ba13508addde14f0744b982cb
Pedro Balage <pedrobalage@gmail.com>
2018-03-21 22:26:19 +0000

    fixed some complaints from codacy

--- lxmls/classifiers/svm.py
+++ lxmls/classifiers/svm.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 import lxmls.classifiers.linear_classifier as lc
 from lxmls.util.my_math_utils import *
@@ -6,7 +5,7 @@ from lxmls.util.my_math_utils import *
 
 class SVM(lc.LinearClassifier):
 
-    def __init__(self, nr_epochs=10, initial_step=1.0, alpha=1.0, regularizer=1.0):
+    def __init__(self, nr_epochs=10, initial_step=1.0, alpha=1.0, regularizer=1.0, seed=1):
         lc.LinearClassifier.__init__(self)
         self.trained = False
         self.nr_epochs = nr_epochs
@@ -14,16 +13,16 @@ class SVM(lc.LinearClassifier):
         self.initial_step = initial_step
         self.alpha = alpha
         self.regularizer = regularizer
+        # use seed to generate permutation
+        np.random.seed(seed)
 
-    def train(self, x, y, seed=1):
+    def train(self, x, y):
         self.params_per_round = []
         x_orig = x[:, :]
         x = self.add_intercept_term(x)
         nr_x, nr_f = x.shape
         nr_c = np.unique(y).shape[0]
         w = np.zeros((nr_f, nr_c))
-        # use seed to generate permutation
-        np.random.seed(seed)
         perm = np.random.permutation(nr_x)
         # print "Starting Loop"
         t = 0
@@ -39,8 +38,8 @@ class SVM(lc.LinearClassifier):
                 cost_augmented_loss[:, y_true] -= 1
                 y_hat = np.argmax(cost_augmented_loss, axis=1).transpose()
                 # if(y_true != y_hat):
-                objective += 0.5*self.regularizer*l2norm_squared(w) - scores[:, y_true] + cost_augmented_loss[:, y_hat]
-                w *= (1 - self.regularizer*learning_rate)
+                objective += 0.5 * self.regularizer * l2norm_squared(w) - scores[:, y_true] + cost_augmented_loss[:, y_hat]
+                w *= (1 - self.regularizer * learning_rate)
                 w[:, y_true] += learning_rate * x[inst:inst+1, :].transpose()
                 w[:, y_hat] -= learning_rate * x[inst:inst+1, :].transpose()
             self.params_per_round.append(w.copy())
