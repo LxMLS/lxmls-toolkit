42a6b33959427696e92721ec4d01ff16c29c9ae5
zam <zam@pba07.interno.priberam.pt>
2019-03-15 13:50:40 +0000

    add RL version working with 1 batch size bad scores wit larger batches

--- labs/notebooks/reinforcement_learning/RL.py
+++ labs/notebooks/reinforcement_learning/RL.py
@@ -9,71 +9,36 @@ print(data.output_size)
 print(data.maxL)
 
 
-# Alterbative native CuDNN native implementation of RNNs
-from lxmls.deep_learning.pytorch_models.rnn import PolicyRNN
-model = PolicyRNN(
-    input_size=data.input_size,
-    embedding_size=50,
-    hidden_size=20,
-    output_size=data.output_size,
-    learning_rate=0.1,
-    gamma=0.9,
-    RL=True,
-    maxL=data.maxL
-)
-
-num_epochs = 10
 
 import numpy as np
 import time
 
-
+batch_size = 1
 # Get batch iterators for train and test
-train_batches = data.sample('train', batch_size=10)
-dev_set = data.sample('dev', batch_size=10)
-test_set = data.sample('test', batch_size=10)
+train_batches = data.batches('train', batch_size=batch_size)
+dev_set = data.batches('dev', batch_size=batch_size)
+test_set = data.batches('test', batch_size=batch_size)
 
-# # Epoch loop
-# start = time.time()
-# for epoch in range(num_epochs):
-#
-#     # Batch loop
-#     for batch in train_batches:
-#         model.update(input=batch['input'], output=batch['output'])
-#
-#     # Evaluation dev
-#     is_hit = []
-#     for batch in dev_set:
-#         is_hit.extend(model.predict(input=batch['input']) == batch['output'])
-#     accuracy = 100 * np.mean(is_hit)
-#
-#     # Inform user
-#     print("Epoch %d: dev accuracy %2.2f %%" % (epoch + 1, accuracy))
-#
-# print("Training took %2.2f seconds per epoch" % ((time.time() - start) / num_epochs))
-#
-# # Evaluation test
-# is_hit = []
-# for batch in test_set:
-#     is_hit.extend(model.predict(input=batch['input']) == batch['output'])
-# accuracy = 100 * np.mean(is_hit)
-#
-# # Inform user
-# print("Test accuracy %2.2f %%" % accuracy)
-#
-# # Example of sampling
-# print(train_batches[3]['input'])
-# samples, log_probs = model._sample(input=train_batches[3]['input'])
-# samples, log_probs
-print("RL")
+
+
+# Alterbative native CuDNN native implementation of RNNs
+from lxmls.deep_learning.pytorch_models.rnn import FastPytorchRNN
+model = FastPytorchRNN(
+    input_size=data.input_size,
+    embedding_size=50,
+    hidden_size=100,
+    output_size=data.output_size,
+    learning_rate=0.05
+)
+
+
+num_epochs = 15
 # Epoch loop
 start = time.time()
 for epoch in range(num_epochs):
+
     # Batch loop
     for batch in train_batches:
-        #TODO: Use this here to create an RL inside model.update()
-        #samples, log_probs = model._sample(input=batch['input'])  # sample actions and its neg log probs
-        embed()
         model.update(input=batch['input'], output=batch['output'])
 
     # Evaluation dev
@@ -84,13 +49,63 @@ for epoch in range(num_epochs):
 
     # Inform user
     print("Epoch %d: dev accuracy %2.2f %%" % (epoch + 1, accuracy))
-
 print("Training took %2.2f seconds per epoch" % ((time.time() - start) / num_epochs))
-
 # Evaluation test
 is_hit = []
 for batch in test_set:
     is_hit.extend(model.predict(input=batch['input']) == batch['output'])
+accuracy = 100 * np.mean(is_hit)
+
+# Inform user
+print("Test accuracy %2.2f %%" % accuracy)
+
+
+# Alterbative native CuDNN native implementation of RNNs
+from lxmls.deep_learning.pytorch_models.rnn import PolicyRNN
+model = PolicyRNN(
+    input_size=data.input_size,
+    embedding_size=50,
+    hidden_size=100,
+    output_size=data.output_size,
+    learning_rate=0.05,
+    gamma=0.2,
+    RL=True,
+    maxL=data.maxL
+)
+
+num_epochs = 15
+
+import numpy as np
+import time
+
+batch_size = 1
+# Get batch iterators for train and test
+train_batches = data.sample('train', batch_size=batch_size)
+dev_set = data.sample('dev', batch_size=batch_size)
+test_set = data.sample('test', batch_size=batch_size)
+print("RL")
+# Epoch loop
+start = time.time()
+for epoch in range(num_epochs):
+    # Batch loop
+    for batch in train_batches:
+        #TODO: Use this here to create an RL inside model.update()
+        #samples, log_probs = model._sample(input=batch['input'])  # sample actions and its neg log probs
+        model.update(input=batch['input'], output=batch['output'])
+    # Evaluation dev
+    is_hit = []
+    for batch in dev_set:
+        loss = model.predict_loss(batch['input'], batch['output'])
+        is_hit.extend(loss)
+    accuracy = 100 * np.mean(is_hit)
+    # Inform user
+    print("Epoch %d: dev accuracy %2.2f %%" % (epoch + 1, accuracy))
+
+print("Training took %2.2f seconds per epoch" % ((time.time() - start) / num_epochs))
+# Evaluation test
+is_hit = []
+for batch in test_set:
+    is_hit.extend(model.predict_loss(batch['input'],batch['output']))
 accuracy = 100 * np.mean(is_hit)
 
 # Inform user
