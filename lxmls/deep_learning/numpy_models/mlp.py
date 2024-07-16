Fix signs of backprop to reflect guide

lxmls/deep_learning/numpy_models/mlp.py
@@ -98,7 +98,7 @@ class NumpyMLP(MLP):
         # Initial error is the cost derivative at the last layer (for cross
         # entropy cost)
         I = index2onehot(output, num_clases)
-        error = (I - prob_y) / num_examples
+        error = - (I - prob_y) / num_examples
         errors.append(error)
 
         # Backpropagate through each layer
@@ -123,13 +123,13 @@ class NumpyMLP(MLP):
             # Weight gradient
             weight_gradient = np.zeros(self.parameters[n][0].shape)
             for l in range(num_examples):
-                weight_gradient -= np.outer(
+                weight_gradient += np.outer(
                     errors[n][l, :],
                     layer_inputs[n][l, :]
                 )
 
             # Bias gradient
-            bias_gradient = -np.sum(errors[n], axis=0, keepdims=True)
+            bias_gradient = np.sum(errors[n], axis=0, keepdims=True)
 
             # Store gradients
             gradients.append([weight_gradient, bias_gradient])