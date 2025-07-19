bacec42d1f0d13744bfd2c6cc976b9f0f4f83b10
Russel Dsouza <rshwndsz@gmail.com>
2025-07-19 08:16:43 +0100

    merge tests from master

--- tests/test_galton_dataset.py
+++ tests/test_galton_dataset.py
@@ -1,10 +1,8 @@
-import sys
-import os
-import pytest
-
-from numpy import array, array_equal, allclose
 import matplotlib
-matplotlib.use('Agg')
+import pytest
+from numpy import allclose, array
+
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 from lxmls.readers import galton
@@ -12,7 +10,7 @@ from lxmls.readers import galton
 tolerance = 1e-5
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def galton_data():
     return galton.load()
 
@@ -27,11 +25,14 @@ def test_galton_data(galton_data):
     assert allclose(std, expected_std, tolerance)
 
     n, bins, _ = plt.hist(galton_data)
-    expected_n = [array([  0.,  14.,  23.,  66., 289., 219., 183.,  68.,  43.,  23.]), array([ 12.,  32., 107., 117., 138., 120., 167., 163.,  41.,  31.])]
+    expected_n = [
+        array([0.0, 14.0, 23.0, 66.0, 289.0, 219.0, 183.0, 68.0, 43.0, 23.0]),
+        array([12.0, 32.0, 107.0, 117.0, 138.0, 120.0, 167.0, 163.0, 41.0, 31.0]),
+    ]
     expected_bins = array([61.7, 62.9, 64.1, 65.3, 66.5, 67.7, 68.9, 70.1, 71.3, 72.5, 73.7])
     assert allclose(n, expected_n, tolerance)
     assert allclose(bins, expected_bins, tolerance)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main([__file__])
